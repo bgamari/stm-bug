29829ce3f56c5d6f8ffc42c035f53e9848fbde75
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-06-20 14:04:17 +0300

    More simplifications

--- Main.hs
+++ Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DefaultSignatures          #-}
 {-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE ExistentialQuantification  #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE FunctionalDependencies     #-}
@@ -12,28 +11,22 @@
 module Main where
 
 import           Control.Concurrent.STM
-import           Data.Typeable
-import           GHC.Generics
 
 data Free f a = Pure a | Free (f (Free f a))
-  deriving (Typeable, Generic, Generic1)
 
 instance Functor f => Functor (Free f) where
   fmap f = go where
     go (Pure a)  = Pure (f a)
     go (Free fa) = Free (go <$> fa)
-  {-# INLINE fmap #-}
 
 instance Functor f => Applicative (Free f) where
   pure = Pure
-  {-# INLINE pure #-}
-  Pure a <*> Pure b = Pure $ a b
-  Pure a <*> Free mb = Free $ fmap a <$> mb
-  Free ma <*> b = Free $ (<*> b) <$> ma
+  Pure a <*> Pure b = Pure (a b)
+  Pure a <*> Free mb = Free (fmap a <$> mb)
+  Free ma <*> b = Free ((<*> b) <$> ma)
 
 instance Functor f => Monad (Free f) where
   return = pure
-  {-# INLINE return #-}
   Pure a >>= f = f a
   Free m >>= f = Free ((>>= f) <$> m)
 
@@ -50,7 +43,6 @@ class Monad m => MonadFree f m | m -> f where
 
 instance Functor f => MonadFree f (Free f) where
   wrap = Free
-  {-# INLINE wrap #-}
 
 data SuspendF v next
   = forall r. StepSTM (STM r) (r -> next)
@@ -62,25 +54,25 @@ newtype Widget v a = Widget { step :: Free (SuspendF v) a }
   deriving (Functor, Applicative, Monad)
 
 effect :: STM a -> Widget v a
-effect a = Widget $ liftF $ StepSTM a id
+effect a = Widget (liftF (StepSTM a id))
 
 io :: IO a ->  Widget v a
-io a = Widget $ liftF $ StepIO a id
+io a = Widget (liftF (StepIO a id))
 
 comb :: Monoid v => [Free (SuspendF v) a] -> Widget v a
-comb vs = io $ do
+comb vs = io (do
   rs <- mapM (go mempty) vs
-  undefined
+  undefined)
 
 go :: v -> Free (SuspendF v) a -> IO (Either a (v, STM (Free (SuspendF v) a)))
 go v (Free (StepIO a next))  = a >>= go v . next
-go v (Free (StepSTM a next)) = pure $ Right (v, a >>= pure . next)
-go _ (Pure a)                = pure $ Left a
+go v (Free (StepSTM a next)) = pure (Right (v, a >>= pure . next))
+go _ (Pure a)                = pure (Left a)
 
 runWidget :: Widget String a -> IO a
 runWidget (Widget w) = case w of
   Free (StepIO io next) -> do
     a <- io
-    runWidget $ Widget $ next a
+    runWidget (Widget (next a))
 
-main = runWidget $ comb $ map step (replicate 10000000 (effect retry))
+main = runWidget (comb (map step (replicate 10000000 (effect retry))))
