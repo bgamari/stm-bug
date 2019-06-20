4f90186b10b9897ad5360e9677c64b638b06063b
Ben Gamari <ben@smart-cactus.org>
2019-06-20 10:03:33 -0400

    Ryan's reproducer

--- Main.hs
+++ Main.hs
@@ -1,72 +1,38 @@
-{-# LANGUAGE CPP          #-}
-{-# LANGUAGE DefaultSignatures          #-}
-{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE CPP  #-}
 {-# LANGUAGE ExistentialQuantification  #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE FunctionalDependencies     #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE StandaloneDeriving         #-}
+module Main (main) where
 
-module Main where
-
-import           Control.Concurrent.STM
+import Control.Concurrent.STM
 
 data Free f a = Pure a | Free (f (Free f a))
 
-instance Functor f => Functor (Free f) where
-  fmap f = go' where
-    go' (Pure a)  = Pure (f a)
-    go' (Free fa) = Free (go' <$> fa)
+data SuspendF a
+  = forall r. StepSTM (STM r)
+  | forall r. StepIO (IO r)
 
-instance Functor f => Applicative (Free f) where
-  pure = Pure
-  Pure a <*> Pure b = Pure (a b)
-  Pure a <*> Free mb = Free (fmap a <$> mb)
-  Free ma <*> b = Free ((<*> b) <$> ma)
+effect :: STM a -> Free SuspendF a
+effect a = Free $ StepSTM a
 
-instance Functor f => Monad (Free f) where
-  return = pure
-  Pure a >>= f = f a
-  Free m >>= f = Free ((>>= f) <$> m)
+io :: IO a -> Free SuspendF a
+io a = Free $ StepIO a
 
-data SuspendF v next
-  = forall r. StepSTM (STM r) (r -> next)
-  | forall r. StepIO (IO r) (r -> next)
+comb :: [Free SuspendF a] -> Free SuspendF a
+comb vs = io $ do
+  _ <- mapM go vs
+  undefined
 
-instance Functor (SuspendF v) where
-  fmap f (StepSTM y g) = StepSTM y (f . g)
-  fmap f (StepIO y g) = StepIO y (f . g)
+go :: Free SuspendF a -> IO (STM ())
+go (Free (StepIO a))  = a >>= \_ -> go $ Pure undefined
+go (Free (StepSTM a)) = pure $ a >>= \_ -> pure ()
+go (Pure _)           = pure $ pure ()
 
-newtype Widget v a = Widget { step :: Free (SuspendF v) a }
-  deriving (Functor, Applicative, Monad)
-
-effect :: STM a -> Widget v a
-effect a = Widget (Free $ fmap return (StepSTM a id))
-
-io :: IO a ->  Widget v a
-io a = Widget (Free $ fmap return (StepIO a id))
-
-comb :: Monoid v => [Free (SuspendF v) a] -> Widget v a
-comb vs = io (do
-  _ <- mapM (go mempty) vs
-  undefined)
-
-go :: v -> Free (SuspendF v) a -> IO (Either a (v, STM (Free (SuspendF v) a)))
-go v (Free (StepIO a next))  = a >>= go v . next
-go v (Free (StepSTM a next)) = pure (Right (v, a >>= pure . next))
-go _ (Pure a)                = pure (Left a)
-
-runWidget :: Widget String a -> IO a
-runWidget (Widget w) = case w of
-  Free (StepIO action next) -> do
-    a <- action
-    runWidget (Widget (next a))
-  Free (StepSTM _ _ ) -> error "hi"
-  Pure r -> return r
+runWidget :: Free SuspendF a -> IO a
+runWidget w = case w of
+  Free (StepIO io) -> do
+    _ <- io
+    undefined
 
 #if defined(GOOD)
 main :: IO ()
 #endif
-main = runWidget (comb (map step (replicate 1000000 (effect retry))))
+main = runWidget $ comb $ replicate 10000000 (effect retry)
